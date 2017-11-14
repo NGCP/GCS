f7a66e785c8d5c54a786f6006047a70d1fa959dc
Luis729 <luisanduis729@gmail.com>
2017-11-13 17:57:12 -0800

    Map modifications, removed Node stuff
    I need Node back, I dont think Chrome likes what I am doing now

--- mapElement/map.js
+++ mapElement/map.js
@@ -2,42 +2,15 @@
  * 	JavaScript file/method definitions for MAPCONTAINER.HTML
  *	Please keep map, locations, and markers outside of functions to allow functions to access them
  */
+ 
+var mapContainer;
+window.onload = function() {
+	let mapFrame = document.getElementById('mapContainer');
+	mapContainer = mapFrame.contentDocument || mapFrame.contentWindow.document;
+}
 
 const calpoly = {lat: 35.306205, lng: -120.662227}, cpp = {lat: 34.0575651, lng: -117.8229297};
 var center = calpoly; // This is the location if the user location will not be found (in time), see below for big comment block
-
-/* 
- *	Methods that will be run immediate when GUI opens
- */
-window.onload = function() {
-	updateCenterToUserLocation();
-	setTimeout(function() {			// 5000 ms delay to give Google Maps 5 seconds to retrieve user location.
-		loadMap('map');				
-	}, 5000);	
-	
-	// Reason for setTimeout(function(), 5000):					
-	// When setCenterToUserLocation() will be called, webpage reads next line
-	// already, but Google Maps did not update 'var center' to user location yet.
-	// Without the delay, the markers will not be placed in the user location
-	// except if the user is in the location 'var center' is initialized to.
-	// The delay only adds time for Google Maps to update 'var center' before
-	// the map is loaded. However, if the map loads before Google Maps updates,
-	// then the markers are going to be in the wrong location.
-}
-
-/*
- *	Gets user location, pans to that location, and creates markers in that location
- */
-function updateCenterToUserLocation() {
-	if (navigator.geolocation) { 									// browser supports location services
-		navigator.geolocation.getCurrentPosition(function(p) {		// user location is found
-			center = {
-				lat: p.coords.latitude,
-				lng: p.coords.longitude
-			};
-		});
-	}
-}
 
 var map, infoWindow;
 
@@ -59,7 +32,8 @@ function loadMap(htmlID) {
  *	@param id	id attribute of HTML element
  */
 function createMap(id) {
-	map = new google.maps.Map(document.getElementById(id), {
+	alert(mapContainer.getElementById(id));
+	map = new google.maps.Map(mapContainer.getElementById(id), {
 		center: center,
 		zoom: 18,
 		mapTypeId: 'satellite',
@@ -76,20 +50,28 @@ function createMap(id) {
  *	Note: markers are the icons of the vehicles on the map
  */
 function addMarkers() {
-	let list = ['uav1', 'uav2', 'ugv1', 'ugv2']; // list of image files w/o .png
+	let list = ['uav1', 'uav2', 'ugv1', 'ugv2'];
 	for (let i = 0; i < list.length; i++) {
+		// create image
 		let icon = {
 			url: '../resources/vehicle-icons/' + list[i] + '.png',
 			scaledSize: new google.maps.Size(60, 60),
 			anchor: new google.maps.Point(25, 25)
 		};
 		
+		// create marker
 		markers[i] = new google.maps.Marker({
 	  		position: center,
 	  		map: map,
-	  		icon: icon
+	  		icon: icon,
+	  		draggable: true
 		});
 		markers[i].index = i;
+		
+		// create marker listener
+		markers[i].addListener('click', function() {
+			updateVehicleStatus(markers[i]); // this method is on vehicleStatus.js
+		});
 	}
 }
 
@@ -98,17 +80,32 @@ function addMarkers() {
  */
 function addButtons() {
 	// create 'Center Map' button
-	let centerMapButton = document.createElement('button');
+	let centerMapButton = mapContainer.createElement('button');
 	centerMapButton.id = 'center';
 	centerMapButton.innerHTML = 'Center Map';
 	centerMapButton.index = 1;
 	map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerMapButton);
 	
+	// create 'Center Map' button listener
 	centerMapButton.addEventListener('click', function() {
 			map.panTo(center);
 	});
 	// create 'Show Vehicle Information' buttons
 	// IN PROGRESS
+}
+
+/*
+ *	Gets user location, pans to that location, and creates markers in that location
+ */
+function updateCenterToUserLocation() {
+	if (navigator.geolocation) { 									// browser supports location services
+		navigator.geolocation.getCurrentPosition(function(p) {		// user location is found
+			center = {
+				lat: p.coords.latitude,
+				lng: p.coords.longitude
+			};
+		});
+	}
 }
 
 /*
