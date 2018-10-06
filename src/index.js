8d23bd871964736f4ccc893e295b75984ef449d6
Paul Matthew Barrameda <luisanduis729@gmail.com>
2018-10-05 20:19:08 -0700

    Update 10/5/18
    Temp update, will update again tonight
    
    My computer's being stupid

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+/*
+ * Ignore for now
+ * We will work on this when we finish writing
+ * the GUI for Webpack
+
 const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const w = new BrowserWindow();
   w.maximize();
 
-  w.loadURL('https://www.facebook.com/');
+  w.loadURL();
 }
 
 app.on('ready', createWindow);
+*/
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => <div>Hello React!</div>;
+
+ReactDOM.render(<Index />, document.getElementById('root'));
