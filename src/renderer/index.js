7fc94e1d7b888cd92105bce33bf625c412c0a517
Paul Matthew Barrameda <luisanduis729@gmail.com>
2018-10-09 23:53:50 -0700

    Update 10/9/18
    Updated eslint rules for React. Also minor formatting change to renderer/index.js
    
    Also images seem to be having some sort of issues

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import Log from './log/Log';
-import Map from './map/Map';
-import Mission from './mission/Mission';
-import Vehicle from './vehicle/Vehicle';
+import Log from './log/Log.js';
+import Map from './map/Map.js';
+import Mission from './mission/Mission.js';
+import Vehicle from './vehicle/Vehicle.js';
 
 import './global.css';
 import './index.css';
@@ -13,10 +13,10 @@ class Index extends Component {
   render() {
     return (
       <div className='gridWrapper'>
-        <Map/>
-        <Log/>
-        <Mission/>
-        <Vehicle/>
+        <Map />
+        <Log />
+        <Mission />
+        <Vehicle />
       </div>
     );
   }
