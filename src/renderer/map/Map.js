53f0e2aac713089cfd5e14d99fd53baf3c504638
Paul Matthew Barrameda <luisanduis729@gmail.com>
2018-10-18 00:11:35 -0700

    Added map caching for offline use
    Not tested for long duration of offline use, however it will reload previously loaded map tiles successfully when wifi turns off.

--- src/renderer/map/Map.js
+++ src/renderer/map/Map.js
@@ -2,6 +2,22 @@ import { ipcRenderer } from 'electron';
 import React, { Component } from 'react';
 import { Map, TileLayer } from 'react-leaflet';
 
+/**
+ * Allows our map to cache online using PouchDB. Run before our map is loaded
+ * Credit to https://github.com/MazeMap/Leaflet.TileLayer.PouchDBCached
+ * Note: no need to load leaflet (according to README tutorial) as react-leaflet includes leaflet already
+ */
+function injectCacheIntoWebpage() {
+  const pouchDBScript = document.createElement('script');
+  const pouchDBCacheScript = document.createElement('script');
+
+  pouchDBScript.type = 'text/javascript';
+  pouchDBCacheScript.type = 'text/javascript';
+
+  document.body.appendChild(pouchDBScript);
+  document.body.appendChild(pouchDBCacheScript);
+}
+
 export default class MapContainer extends Component {
   constructor(props) {
     super(props);
@@ -14,6 +30,7 @@ export default class MapContainer extends Component {
 
     ipcRenderer.on('updateMapLocation', (event, data) => this.updateMapLocation(data));
 
+    injectCacheIntoWebpage();
     this.updateMapLocation = this.updateMapLocation.bind(this);
   }
 
@@ -24,12 +41,17 @@ export default class MapContainer extends Component {
   render() {
     const { latitude, longitude, zoom } = this.state;
     const center = [latitude, longitude];
-
     return (
-      <Map className='mapContainer container' center={center} zoom={zoom}>
+      <Map
+        className='mapContainer container'
+        center={center}
+        zoom={zoom}
+      >
         <TileLayer
           attribution='&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          useCache={true}
+          crossOrigin={true}
         />
       </Map>
 
