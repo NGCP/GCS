91fdfe7ddb8deedfae7ed100657067bcd994d05c
tnleang <st.leang@gmail.com>
2018-10-20 08:12:22 -0700

    vehicle Simple table head

--- src/renderer/vehicle/Vehicle.js
+++ src/renderer/vehicle/Vehicle.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
 
+import './vehicle.css';
+
+const WIDTH = {
+  id: 0.2,
+  name: 0.4,
+  battery: 0.4,
+};
+
 export default class VehicleContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      vehicle: '',
+    };
+  }
+
+
   render() {
-    return <div className='vehicleContainer container'></div>;
+    this.setState();
+    return (
+      <div className='vehicleContainer container'>
+        <table className='vehicleData'>
+          <tr className='vehicleTableHeader'>
+            <th style={{ width: `${100 * WIDTH.id}%` }}>ID</th>
+            <th style={{ width: `${100 * WIDTH.name}%` }}>NAME</th>
+            <th style={{ width: `${100 * WIDTH.battery}%` }}>BATTERY</th>
+          </tr>
+        </table>
+      </div>
+    );
   }
 }
