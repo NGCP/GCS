e0947f30e50f6d50f4cc42200409ba0d4aacaf85
Paul Matthew Barrameda <luisanduis729@gmail.com>
2018-11-15 15:50:47 -0800

    Update xbee.js to test serialport capability on macOS/Windows/Linux

--- src/util/xbee.js
+++ src/util/xbee.js
@@ -1,25 +1,39 @@
 import SerialPort from 'serialport';
-import { XBeeAPI } from 'xbee-api';
+import { costants as C, XBeeAPI } from 'xbee-api';
 
-const serialport = new SerialPort('COM5', { baudRate: 57600 });
-const xbee = new XBeeAPI({ api_mode: 1 });
+const port = new SerialPort('/dev/tty.usbserial-ddd', { baudRate: 57600 });
+const xbeeAPI = new XBeeAPI({ api_mode: 1 });
 
+port.write('main screen turn on', err => {
+  if (err) {
+    console.error('error on write', err.message);
+  } else {
+    console.log('message written');
+  }
+});
+
+port.on('error', err => {
+  console.error('Error:', err.message);
+});
+
+/**
 let id;
 
-serialport.pipe(xbee.parser);
-xbee.builder.pipe(serialport);
+serialport.pipe(xbeeAPI.parser);
+xbeeAPI.builder.pipe(serialport);
 
 serialport.on('open', () => {
   setInterval(() => {
-    id = xbee.nextFrameId();
+    id = xbeeAPI.nextFrameId();
 
-    xbee.builder.write({
+    xbeeAPI.builder.write({
       id: id,
-      type: 0x10,
+      type: C.FRAME_TYPE.ZIGBEE_TRANSMIT_REQUEST,
       destination64: '0013A20040917A31',
-      data: 'heeee',
+      data: 'TxData0A',
     });
   }, 1000);
 });
 
-xbee.parser.on('data', frame => {}); // eslint-disable-line no-empty-function
+xbeeAPI.parser.on('data', console.log);
+*/
