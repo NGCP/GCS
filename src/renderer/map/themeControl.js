45f88037ab7be40c9799234ba747a7a29e51883c
tnleang <st.leang@gmail.com>
2019-02-22 12:27:06 -0800

    set up for switch button

---
+++ src/renderer/map/themeControl.js
@@ -0,0 +1,27 @@
+import { ipcRenderer } from 'electron';
+import React, { Component } from 'react';
+import Control from 'react-leaflet-control';
+
+/**
+ * switch button to control the theme of UI
+ * when clicked the theme changes
+ *  Dark -> Light
+ *  Light -> Dark
+ */
+
+export default class ThemeControl extends Component {
+  onClick = () => {
+    ipcRenderer.send('post', 'switchTheme');
+  };
+
+  render() {
+    return (
+      <Control className='leaflet-bar' position='topright'>
+        <a
+          className='theme-control'
+          onClick={this.onClick}
+        />
+      </Control>
+    );
+  }
+}
