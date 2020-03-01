2888856f6924a490da4da90f49284f86ac39463d
Paul Matthew Barrameda <luisanduis729@gmail.com>
2020-02-29 17:05:34 -0800

    Update Travis CI to add webhooks and remove cache
     ## Why is the change being made?
    
    This change is made because webhooks from GitHub to Discord was annoying
    with pushes/pull requests/etc and was not necessary. The necessary
    update to developers is the build status of the `master` branch.
    
    This change also addresses the fact that a build has failed to succeed
    due to an invalid cache.
    
     ## What has changed to address the problem?
    
    This change will add a script that will run another script that sends a
    webhook of the build. The webhook URL has to be set up in Discord, and
    made an environmental variable in Travis CI to work.
    
     ## How was this change tested?
    
    This change will be tested by making the `webhook` branch emit to
    Discord and see if it goes through successfully. Then the emitting
    branch will be changed to `master`.
    
    Also, the cache was removed from `.travis.yml`.
    
     ## Related documents, URLs, commits
    
    https://github.com/DiscordHooks/travis-ci-discord-webhook#guide

---
+++ script/webhook.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+  echo "ERROR: Expecting an argument (success/failure) but got none"
+  exit 1
+fi
+
+if [[ $TRAVIS_BRANCH != 'webhook' ]]; then
+  echo "INFO: Not the master branch, will not send webhook"
+  exit 0
+fi
+
+echo "INFO: On master branch, sending webhook"
+wget https://raw.githubusercontent.com/DiscordHooks/travis-ci-discord-webhook/master/send.sh
+chmod +x send.sh
+./send.sh $1 $WEBHOOK_URL
